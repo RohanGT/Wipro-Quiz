d1a63efb1c62f49aee81ee9d5849f5ec64ffeee5
Sarthak <sarthak0699@gmail.com>
2018-09-17 12:17:29 +0530

    i'm sorry about the final 1

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
 	<title>Quiz Registration</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-   <link href="style.css?<?=filemtime("style.css")?>" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="style.css?<?=filemtime("style.css")?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
@@ -17,37 +17,50 @@
         <h1>Registration Form</h1>
     </header>
     <main>
-    <section>
+    <div class="Grid">
             <div>
-                <h2> Welcome to the Wipro Quiz!</h2>
+                <div class="h2"><h2> Welcome to the Wipro Quiz!</h2></div>
                 <p class="wipro">
-                	<img src="wipro.png" id="wipro" alt="Wipro">
+                	<div class="wiprod"><img src="wipro.png" id="wipro" alt="Wipro"></div>
                    	<div id="ins"><a href="Document.pdf" download >Click to Download Instructions</a></div>
                 </p>
             </div>
-        </section>
 		<form method="POST" enctype="multipart/form-data" action='<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]) ?>' >
-			<div class="f1">
+				<div class="lname"><label for="name">Name:</label></div>
+				<div class="f2 fname">
 					<input type="text" name="name" class="i" placeholder="Name" required value="<?php if(!empty($name)) echo $name?>">
 				</div>
-				<div class="f1">
-				
-					<input type="number" name="regno" class="i" placeholder="Registration Number" min="160000000" max="190000000" required title="Required" value="<?php if(!empty($regno)) echo $regno?>">
+				<div class="lregno"><label for="regno">Registration No:</label></div>
+				<div class="f2 fregno">
+					<input type="number" name="regno" class="i" placeholder="Registration Number" min="130000000" max="190000000" required title="Required" value="<?php if(!empty($regno)) echo $regno?>">
 				</div>
-			<div class="f1">
+				<div class="lbranch"><label for="branch">Branch:</label></div>
+				<div class="f2 fbranch">
 					<input type="text"  class="i" name="branch" placeholder="Branch" required value="<?php if(!empty($branch)) echo $branch?>">
 				</div>
-
-				<div class="f1">
-					<input type="number" name="semester" class="i" placeholder="Semester" min="1" max="8" required title="Required" value="<?php if(!empty($semester)) echo $semester?>">
+				<div class="semf2">
+					<div class="lsem"><label>Semester:</label></div>
+					<select name="semester" id="sem2">
+						<option value=1>Semester 1</option>
+						<option value=2>Semester 2</option>
+						<option value=3>Semester 3</option>
+						<option value=4>Semester 4</option>
+						<option value=5>Semester 5</option>
+						<option value=6>Semester 6</option>
+						<option value=7>Semester 7</option>
+						<option value=8>Semester 8</option>
+						<option value=9>Other</option>
+					</select>
 				</div>
-			<div class="f1" >
+				<div class="lemail"><label for="email">Email:</label></div>
+				<div class="f2 femail">
 					<input type="email" class="i" name="email" placeholder="Email" required value="<?php if(!empty($email)) echo $email?>">
 				</div>
-				<div class="f1">
+				<div class="lphone"><label for="phone">Phone No:</label></div>
+				<div class="f2 fphone">
 					<input type="number"class="i"  name="phone" placeholder="Mobile-No" min="1000000000" max="9999999999" required value="<?php if(!empty($phone)) echo $phone?>">
 				</div>
-			<div>
+			<div class="finald">
 				<span class="final"><?php echo $success?></span>
 				<span class="final"><?php echo $error?></span>
 			</div>
@@ -55,6 +68,7 @@
 				<input type="submit" name="submit" id="submit" value="Submit">
 			</div>
 		</form>
+		</div>	
 		</main>
 </body>
 </html>
